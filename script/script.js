e21e5742a614b68e112c76ae832eb7cc6f168ba6
Ishindoutai <jseacello@gmail.com>
2022-01-11 17:34:45 -0300

    DrawFood

--- script/script.js
+++ script/script.js
@@ -7,6 +7,11 @@ snake[0] = {
     y: 8 * box
 }
 let direction = right = "right";
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
     
 
 function criarBG() {
@@ -19,6 +24,11 @@ function criarSnake(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -38,6 +48,7 @@ function iniciarJogo(){
 
     criarBG();
     criarSnake();
+    drawFood();
 
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;
